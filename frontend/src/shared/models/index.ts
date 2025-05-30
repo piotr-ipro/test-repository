b176cd82be4ea6b03f8a72145ac5d3b65706e2a2
piotr-ipro <piotr@ipro-elearning.com>
2025-05-30 13:21:31 +0200

    Add chat history page and hook

--- frontend/src/shared/models/index.ts
+++ frontend/src/shared/models/index.ts
@@ -2,3 +2,4 @@ export * from './BackgroundTask.model';
 export * from './LlmImage.model';
 export * from './RagAskResponse.model';
 export * from './Route.model';
+export * from './ChatHistory.model';
